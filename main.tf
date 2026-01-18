09b6d752dadfad01d1a809f41ccde58088679d28
VinithaAshokSingh <vinithashokunal@gmail.com>
2026-01-19 01:04:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "jenkins-tf-vm-vini"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2ats_v2"
+  size                = "Standard_D2s_v3"
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id
