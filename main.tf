7cf20b9572321a17cb6c3b1f774ace0f4e7ccbb1
VinithaAshokSingh <vinithashokunal@gmail.com>
2026-01-20 11:24:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = var.ssh_public_key
+    public_key = file("/home/vinitha/.ssh/id_rsa.pub")
   }
 
   os_disk {
